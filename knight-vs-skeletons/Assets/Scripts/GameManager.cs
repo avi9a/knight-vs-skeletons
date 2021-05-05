1c7916a6188a547d367afd6068324927bd7641a8
avi9a <73037370+avi9a@users.noreply.github.com>
2021-05-05 21:05:36 +0300

    feat: add EndLevel method

--- knight-vs-skeletons/Assets/Scripts/GameManager.cs
+++ knight-vs-skeletons/Assets/Scripts/GameManager.cs
@@ -6,6 +6,15 @@ public class GameManager : MonoBehaviour
 {
     public float timer;
     public void EndLevel(CanvasGroup canvas, bool restart) {
-
+        timer += Time.deltaTime / 2;
+        canvas.alpha = timer / 1;
+        if (timer > 1) {
+            if (restart) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            else {
+                Application.Quit();
+            }
+        }
     }
 }
